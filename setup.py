3f78188706afff3701b9739833e35ff5b0bfe9bd
Ben Lebherz <git@benleb.de>
2020-01-07 21:16:14 +0100

    locally it works

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='adutils',
       packages=['adutils'],
       zip_safe=False,
       platforms='any',
-      use_scm_version=True,
+      use_scm_version={'version_scheme': 'python-simplified-semver'},
       setup_requires=['setuptools_scm'],
       classifiers=[
           "Topic :: Software Development",
