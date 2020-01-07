5d624a570be963bde10de992840ff3303b09443b
Ben Lebherz <git@benleb.de>
2020-01-07 07:51:21 +0100

    project config

---
+++ setup.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+from setuptools import setup
+
+setup(name='adutils',
+      version='0.4.6',
+      license='MIT',
+      url='http://github.com/benleb/adutils',
+      author='Ben Lebherz',
+      author_email='git@benleb.de',
+      description='Helper and utilities for AppDaemon apps',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      packages=['adutils'],
+      zip_safe=False,
+      platforms='any',
+      )
